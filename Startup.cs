5bcef827c0802cab9826847a544b20589e8cb860
SiJiL82 <sven.lowry@gmail.com>
2021-06-18 18:39:21 +0100

    Added Google authentication

--- Startup.cs
+++ Startup.cs
@@ -34,6 +34,18 @@ namespace Rephository
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
+
+            #region "Authentication Providers"
+            services.AddAuthentication()
+                .AddGoogle(options =>
+                {
+                    IConfigurationSection googleAuthNSection =
+                        Configuration.GetSection("Authentication:Google");
+
+                    options.ClientId = googleAuthNSection["ClientId"];
+                    options.ClientSecret = googleAuthNSection["ClientSecret"];
+                });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
